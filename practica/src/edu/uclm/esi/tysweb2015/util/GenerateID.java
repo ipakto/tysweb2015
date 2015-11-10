116a17d20648c88a4245d5a5d9a5ad00d839a11b
Paco <Paco@Paco>
2015-11-10 19:17:49 +0100

    1

--- practica/src/edu/uclm/esi/tysweb2015/util/GenerateID.java
+++ practica/src/edu/uclm/esi/tysweb2015/util/GenerateID.java
@@ -5,11 +5,12 @@ import java.security.SecureRandom;
 import java.util.UUID;
 
 public class GenerateID {
-
+//Metodo 1
   public static String generate() {
 	SecureRandom r=new SecureRandom();
     return new BigInteger(130, r).toString(32);
   }
+  //Metodo 2
   public static String generateUUID(){
 	  return UUID.randomUUID().toString().replaceAll("-", "");
   }
