f735f27827f9bf3bef15371d5157385d4bc7c0eb
Paco <Paco@Paco>
2015-11-11 17:48:30 +0100

    Añadidos los TestCase de la clase del 11/11

--- practica/src/edu/uclm/esi/tysweb2015/dao/Broker.java
+++ practica/src/edu/uclm/esi/tysweb2015/dao/Broker.java
@@ -2,11 +2,14 @@ package edu.uclm.esi.tysweb2015.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
 
 public class Broker {
 	private static Broker yo;
-	String url="jdbc:mysql://localhost:3306/tysweb2015?noAccessToProcedureBodies=true";
+	static String url="jdbc:mysql://localhost:3306/tysweb2015?noAccessToProcedureBodies=true";
 	private Broker() throws ClassNotFoundException{
 		Class.forName("com.mysql.jdbc.Driver");
 	}
@@ -15,16 +18,39 @@ public class Broker {
 			yo=new Broker();
 		return yo;
 	}
-	public Connection getConnectionSeleccion() throws SQLException {
+	public static Connection getConnectionSeleccion() throws SQLException {
 		//return DriverManager.getConnection(url,"selectorTSW2015","");
 		return DriverManager.getConnection(url,"root","");
 	}
-	public Connection getConnectionInsercion() throws SQLException {
+	public static Connection getConnectionInsercion() throws SQLException {
 		//return DriverManager.getConnection(url,"inserterTyS2015","inserterTyS2015");
 		return DriverManager.getConnection(url,"root","");
 	}
 	public Connection getConnection(String userName, String pwd) throws SQLException {
 		// TODO Auto-generated method stub
 		return DriverManager.getConnection(url,userName,pwd);
+	}
+	
+	public static Connection getDB(String email, String pwd) throws Exception {
+		Connection bd=getConnectionSeleccion();
+		try{
+			String sql="SELECT id FROM Usuarios WHERE email=?";
+			PreparedStatement p=bd.prepareStatement(sql);
+			p.setString(1, email);
+			ResultSet rs=p.executeQuery();
+			Connection result=null;
+			if(rs.next()){
+				int id=rs.getInt(1);
+				String userName="tysweb2015"+id;
+				result=DriverManager.getConnection(url,userName,pwd);
+				rs.close();
+			}else throw new Exception ("Login o password inválidos");
+			return result;
+		}catch(Exception e){
+			throw e;
+		}
+		finally{
+			bd.close();
+		}
 	}
 }
---
+++ practica/src/edu/uclm/esi/tysweb2015/dao/test/BrokerTest.java
@@ -0,0 +1,128 @@
+package edu.uclm.esi.tysweb2015.dao.test;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Random;
+
+import org.junit.Test;
+
+import edu.uclm.esi.tysweb2015.dao.Broker;
+
+
+public class BrokerTest {
+
+	
+	public void testAperturaDeConexiones() throws Exception {
+		Broker bd=Broker.get();
+		int i=1;
+		boolean error=false;
+		do{
+			try{
+				bd.getDB("pepe@pepe.com","1234");
+			}catch(SQLException e){
+				error=true;
+			}
+			System.out.println(i++);
+		}while(!error);
+		fail("Not yet implemented");
+	}
+	
+	public void testTiemposDeInsercion(){
+		String sql="INSERT INTO anuncios (descripcion,idCategoria,idAnunciante)"+"values(?,?,?);";
+		//Método 1:sobre una conexión abierta
+		try{
+			long ti=System.currentTimeMillis();
+			Connection bd=Broker.get().getConnectionInsercion();
+			PreparedStatement p=bd.prepareStatement(sql);
+			for(int i=0;i<500;i++){
+				p.setString(1, "anuncio de una moto de Prueba"+i);
+				p.setInt(2, 1);
+				p.setInt(3, 33);
+				p.executeUpdate();
+			}
+			bd.close();
+			long tf=System.currentTimeMillis();
+			System.out.println("Tiempo conexión abierta"+(tf-ti)/1000);
+		}catch(Exception e){
+			fail("Excepcion inesperada; "+e);
+		}
+		//Método 1:sobre una conexión que vamos abriendo y cerrando
+		try{
+			long ti2=System.currentTimeMillis();
+			for(int i=0;i<500;i++){
+				Connection bd=Broker.get().getConnectionInsercion();
+				PreparedStatement p=bd.prepareStatement(sql);
+				p.setString(1, "anuncio de una moto de Prueba"+i);
+				p.setInt(2, 1);
+				p.setInt(3, 33);
+				p.executeUpdate();
+				bd.close();
+			}
+			long tf2=System.currentTimeMillis();
+			System.out.println("Tiempo conexión abierta"+(tf2-ti2)/1000);
+		}catch(Exception e){
+			fail("Excepcion inesperada; "+e);
+		}
+	}
+ 
+	public void testInsertarEnPrueba(){
+		String sql="INSERT INTO prueba (cadena1, cadena2) values(?,?);";
+		//Método 1:sobre una conexión abierta
+		try{
+			long ti=System.currentTimeMillis();
+			Connection bd=Broker.get().getConnectionInsercion();
+			PreparedStatement p=bd.prepareStatement(sql);
+			String cadenaAleatoria;
+			for(int i=0;i<300;i++){
+				cadenaAleatoria=getCadenaAleatoria();
+				p.setString(1,cadenaAleatoria);
+				p.setString(2, cadenaAleatoria);
+				p.executeUpdate();
+			}
+			bd.close();
+			long tf=System.currentTimeMillis();
+			System.out.println("Tiempo de inserción: "+(tf-ti)/1000);
+		}catch(Exception e){
+			fail("Excepcion inesperada; "+e);
+		}
+	}
+	@Test
+	public void testBusquedaEnPrueba(){
+		String sql="SELECT * FROM prueba order by cadena1";
+		String sql2="SELECT * FROM prueba order by cadena2";
+		try{
+			long ti=System.currentTimeMillis();
+			Connection bd=Broker.get().getConnectionSeleccion();
+			PreparedStatement p=bd.prepareStatement(sql);
+			ResultSet r=p.executeQuery();
+			r.next();
+			System.out.println(r.getString(1));
+			long tf=System.currentTimeMillis();
+			System.out.println("Tiempo busqueda1: "+(tf-ti));
+			
+
+			long ti2=System.currentTimeMillis();
+			Connection bd2=Broker.get().getConnectionSeleccion();
+			PreparedStatement p2=bd.prepareStatement(sql);
+			ResultSet r2=p.executeQuery();
+			r2.next();
+			System.out.println(r2.getString(1));
+			long tf2=System.currentTimeMillis();
+			System.out.println("Tiempo busqueda2: "+(tf2-ti2));
+		}catch(Exception e){
+			fail("Excepcion inesperada; "+e);
+		}
+	}
+	private String getCadenaAleatoria() {
+		String r="";
+		int longitud=new Random().nextInt(255)+1;
+		for(int i=0;i<longitud;i++){
+			r+=(char)( new Random().nextInt(100)+65);
+		}
+		return r;
+	}
+}
